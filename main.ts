80a14ee5aa7302e207d48b102611812f86393181
M4ttAuger <fanny.93.mr@gmail.com>
2025-07-31 00:53:47 +0200

    add alternative method to calculate sum and average of an array

--- main.ts
+++ main.ts
@@ -8,3 +8,17 @@ for (let i = 0; i < array.length; i++) {
 let media = somma / array.length;
 console.log(`La media è: ${media}`);
 console.log(`la somma è: ${somma}`);
+
+// Oppure
+
+const arr = [1, 2, 3, 4, 5];
+
+let sum = 0;
+for (let num of arr) {
+  sum += num;
+}
+
+const med = sum / arr.length;
+
+console.log(`La media è: ${med}`);
+console.log(`la somma è: ${sum}`);
