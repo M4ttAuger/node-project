e10c8de47a4f44b861ebb8f9927ca6f889eb0d8c
M4tt <fanny.93.mr@gmail.com>
2025-07-30 17:15:33 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+console.log("This is a simple JavaScript program.");
+
+let myVar: number = 3;
+myVar += 10;
+
+console.log("myVar:", myVar);
+
+interface Myinterface {
+  myNumber: number;
+  myString: string;
+}
+
+let myInterface = {
+  myNumber: 10,
+  myString: "Hello, TypeScript!",
+};
+
+console.log("myNumber:", myInterface.myNumber);
+console.log("myString:", myInterface.myString);
